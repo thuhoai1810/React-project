a855d72ef00494bc5dcc31af98aa21d828a5138e
thuhoai1810 <macos@macosnoMacBook-Pro.local>
2022-10-03 17:06:53 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+interface Todos {
+	id: string;
+	todo: string;
+}
+
 function App() {
 	const [job, setJob] = useState<string>("");
-	const [jobs, setJobs] = useState<string[]>(() => {
+	const [jobs, setJobs] = useState<Todos[]>(() => {
 		const storageJob = JSON.parse(localStorage.getItem("jobs") ?? "[]");
 		console.log(storageJob);
 		return storageJob;
 	});
 	useEffect(() => {
-		fetch("https://62e897e393938a545be80ba5.mockapi.io/");
+		fetch("https://62e897e393938a545be80ba5.mockapi.io/Todos").then((res) =>
+			res.json().then((jobs) => {
+				// console.log(jobs);
+				setJobs(jobs);
+			})
+		);
 	}, []);
 	const handleSubmit = () => {
-		setJobs((prev) => {
-			const newJobs = [...prev, job];
+		setJobs(() => {
+			const newJob: Todos = {
+				id: "",
+				todo: job,
+			};
+			const newJobs = [...jobs, newJob];
 
 			// save to local storage
 			const jsonJobs = JSON.stringify(newJobs);
@@ -23,7 +37,6 @@ function App() {
 		});
 		setJob("");
 	};
-
 	return (
 		<div className='App'>
 			<header className='App-header'>
@@ -31,9 +44,10 @@ function App() {
 					<input value={job} onChange={(e) => setJob(e.target.value)} />
 					<button onClick={handleSubmit}>add</button>
 					<ul>
-						{jobs.map((job, index) => (
-							<li key={index}>{job}</li>
-						))}
+						{jobs.map((newJob) => {
+							console.log(job);
+							return <li key={newJob.id}>{newJob.todo}</li>;
+						})}
 					</ul>
 				</div>
 			</header>
